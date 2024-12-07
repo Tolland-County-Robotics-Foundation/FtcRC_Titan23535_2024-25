9a81bbb07a630aa281a994647d0fdd117c827d4b
NotQuite8Bit <advaith@tcrobotics.org>
2024-12-07 15:38:30 -0500

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCodes/FullBoard.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCodes/FullBoard.java
@@ -27,45 +27,45 @@ public class FullBoard {
     DcMotor intake;
     DcMotor intakeWheel;
     //Color Sensor
-    private ColorSensor color;
-    private DistanceSensor distance;
+    ColorSensor color;
+    DistanceSensor distance;
     //Touch Sensor
-    private DigitalChannel touch;
+    DigitalChannel touch;
     //Husky Lens
     HuskyLens husky;
 
     public void init (HardwareMap hardwareMap) {
         //Drivetrain
-        leftFront = BlocksOpModeCompanion.hardwareMap.get(DcMotor.class, "leftFront");
+        leftFront = hardwareMap.dcMotor.get("leftFront");
         leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
-        leftBack = BlocksOpModeCompanion.hardwareMap.get(DcMotor.class, "leftBack");
+        leftBack = hardwareMap.dcMotor.get("leftBack");
         leftBack.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightBack = BlocksOpModeCompanion.hardwareMap.get(DcMotor.class, "rightBack");
+        rightBack = hardwareMap.dcMotor.get("rightBack");
         rightBack.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightFront = BlocksOpModeCompanion.hardwareMap.get(DcMotor.class, "rightFront");
+        rightFront = hardwareMap.dcMotor.get("rightFront");
         rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
         //Linear Slide
-        leftArm = BlocksOpModeCompanion.hardwareMap.get(DcMotor.class, "leftArm");
+        leftArm = hardwareMap.dcMotor.get("leftArm");
         leftArm.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightArm = BlocksOpModeCompanion.hardwareMap.get(DcMotor.class, "rightArm");
+        rightArm = hardwareMap.dcMotor.get("rightArm");
         rightArm.setDirection(DcMotorSimple.Direction.FORWARD);
-        leftS = BlocksOpModeCompanion.hardwareMap.get(Servo.class, "leftS");
+        leftS = hardwareMap.servo.get("leftS");
         leftS.setDirection(Servo.Direction.FORWARD);
-        rightS = BlocksOpModeCompanion.hardwareMap.get(Servo.class, "rightS");
+        rightS = hardwareMap.servo.get("rightS");
         rightS.setDirection(Servo.Direction.REVERSE);
         //Intake
-        intakeWheel = BlocksOpModeCompanion.hardwareMap.get(DcMotor.class, "intake");
+        intakeWheel = hardwareMap.dcMotor.get("intakeWheel");
         intakeWheel.setDirection(DcMotorSimple.Direction.FORWARD);
-        intake = BlocksOpModeCompanion.hardwareMap.get(DcMotor.class, "intake");
+        intake = hardwareMap.dcMotor.get("intake");
         intake.setDirection(DcMotorSimple.Direction.FORWARD);
         //Color Sensor
-        color = BlocksOpModeCompanion.hardwareMap.get(ColorSensor.class, "color");
-        distance = BlocksOpModeCompanion.hardwareMap.get(DistanceSensor.class, "color");
+        color = hardwareMap.get(ColorSensor.class, "color");
+        distance = hardwareMap.get(DistanceSensor.class, "color");
         //Touch Sensor
-        touch = BlocksOpModeCompanion.hardwareMap.get(DigitalChannel.class, "touch");
+        touch = hardwareMap.get(DigitalChannel.class, "touch");
         touch.setMode(DigitalChannel.Mode.INPUT);
         //Husky Lens
-        husky = BlocksOpModeCompanion.hardwareMap.get(HuskyLens.class, "husky");
+        husky = hardwareMap.get(HuskyLens.class, "husky");
     }
 
     //Function Commands (For Autonomous)
