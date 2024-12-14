240645f35985c71a2a6ed9f98dc7124a538a208a
aperera24 <avinperera@gmail.com>
2024-12-14 17:36:12 -0500

    Updated - 12/14/24. updating ServoTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake_Arm_Mechanisms/ServoTest.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.Intake_Arm_Mechanisms;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class ServoTest extends OpMode {
+
+    private Servo motor = null;
+    private double intakePos = 0.0;
+
+    @Override
+    public void init() {
+        motor  = hardwareMap.get(Servo.class, "motor");
+        motor.setDirection(Servo.Direction.FORWARD);
+    }
+
+    @Override
+    public void loop() {
+
+        boolean openButton = gamepad2.left_bumper;
+
+        if (openButton) {
+            intakePos = 0.1;
+        }
+        motor.setPosition(intakePos);
+
+
+    }
+}
