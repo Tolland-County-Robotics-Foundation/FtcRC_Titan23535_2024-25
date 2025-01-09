d27f7a506b965824538d41768095021d8d2c3987
msr09me <146886298+msr09me@users.noreply.github.com>
2025-01-08 21:34:49 -0500

    Revert "Update Distance_Sensor_v1.java"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensor_Mechanisms/Distance_Sensor_v1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensor_Mechanisms/Distance_Sensor_v1.java
@@ -1,18 +1,4 @@
 package org.firstinspires.ftc.teamcode.Sensor_Mechanisms;
 
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-
 public class Distance_Sensor_v1 {
-  DistanceSensor distance;
-
-  public void init (HardwareMap hardwareMap) {
-    distance = hardwareMap.get(DistanceSensor.class, "color");
-  }
-
-  public double getDistance(DistanceUnit du) {
-    return distance.getDistance(du);
-  }
 }
