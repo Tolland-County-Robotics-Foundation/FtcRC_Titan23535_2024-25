433a07c7393c2621ccca035ce5a15d955addb875
ShaydenFTC <shayden@tcrobotics.org>
2025-01-09 20:36:47 -0500

    added update v9 and blinkenv1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCodes/FullGameCode_V9.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCodes/FullGameCode_V9.java
@@ -17,25 +17,24 @@ import java.util.Objects;
 
 @TeleOp(name = "Full Game Code 9", group = "test")
 public class FullGameCode_V9 extends OpMode {
-    // Creating an object from Drive_V1 class
+    /// Creating objects for the drivetrain, color sensor, REV Blinkin, and runtime.
     Drive_v1 drive = new Drive_v1();
     Color_Sensor_v1 color = new Color_Sensor_v1();
     Blinkin_v1 light = new Blinkin_v1();
-    //Update
-    // Creating an object from ElapsedTime class to have run time information
     private final ElapsedTime runtime = new ElapsedTime();
 
-    //Creating variables
+    /// Initializing variables to use across the entire program.
     String speedCap = "Normal";
+    Boolean start = true;
     Boolean rightColor;
-    Boolean redSpy;
+    String redSpy = "none";
     double speed_percentage = 40.0;
 
-    //Creating objects for intake
+    ///Creating objects for the intake mechanisms.
     private CRServo claw        = null;
     private DcMotor intakeArm   = null;
 
-    //Creating objects for long arm
+    /// Creating objects for the linear slide and basket.
     private Servo basket          = null;
     private DcMotor leftArmLift     = null;
     private DcMotor rightArmLift    = null;
@@ -44,59 +43,61 @@ public class FullGameCode_V9 extends OpMode {
     @Override
     public void init()
     {
-        // Wait for the game to start (driver presses PLAY)
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-
         // Resetting runtime
         runtime.reset();
 
-        telemetry.addData(">", "Press Start To Run The Robot" );
         // Use init method to do the initialization of drive object
         drive.init(hardwareMap);
 
-        /// Initialization of intake mechanism ----------------------------------------------
-
+        /// Initialization of intake mechanisms.
         claw        = hardwareMap.get(CRServo.class, "claw");
-        intakeArm   = hardwareMap.get(DcMotor.class, "intakeArm");
-
         claw.setDirection(DcMotorSimple.Direction.FORWARD);
-        intakeArm.setDirection(DcMotorSimple.Direction.FORWARD);
 
+        intakeArm   = hardwareMap.get(DcMotor.class, "intakeArm");
+        intakeArm.setDirection(DcMotorSimple.Direction.FORWARD);
         intakeArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        /// Initialization of long arm mechanism ---------------------------------------------
-
+        /// Initialization of linear slide and basket.
         basket          = hardwareMap.get(Servo.class, "BasketArm");
-        leftArmLift     = hardwareMap.get(DcMotor.class, "LeftArmLift");
-        rightArmLift    = hardwareMap.get(DcMotor.class, "RightArmLift");
-
         basket.setDirection(Servo.Direction.FORWARD);
+
+        leftArmLift     = hardwareMap.get(DcMotor.class, "LeftArmLift");
         leftArmLift.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightArmLift.setDirection(DcMotorSimple.Direction.REVERSE);
-
         leftArmLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        rightArmLift    = hardwareMap.get(DcMotor.class, "RightArmLift");
+        rightArmLift.setDirection(DcMotorSimple.Direction.REVERSE);
         rightArmLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        ///Alliance selection & Blinkin color set
-        telemetry.addData("Are you on the Red Alliance or the Blue Alliance?", "Left trigger for Red, right trigger for Blue");
-        while (true) {
-            if (gamepad1.left_trigger > 0.1) {
-                redSpy = true;
-                break;
-            }
-            if (gamepad1.right_trigger > 0.1) {
-                redSpy = false;
-                break;
-            }
-        }
-
-        light.allianceSet(redSpy);
     }
     @Override
     public void loop()
     {
-        // Set the speed cap for driver 1
+        /// Alliance data.
+        //Sets the telemetry reply for alliance color.
+        String allianceText;
+        if (Objects.equals(redSpy, "red")) {
+            allianceText = "Red";
+        } else if (Objects.equals(redSpy, "blue")) {
+            allianceText = "Blue";
+        } else {
+            allianceText = "Left Trigger for Red, Right Trigger for Blue";
+        }
+
+        telemetry.addData("Alliance Selected", allianceText);
+
+        //Chooses the alliance.
+        if (start) {
+            if (gamepad1.left_trigger > 0.1) {
+                redSpy = "red";
+                start = false;
+            }
+            if (gamepad1.right_trigger > 0.1) {
+                redSpy = "blue";
+                start = false;
+            }
+        }
+
+        /// Sets the speed cap for driver 1.
         if (gamepad1.y) {
             speedCap = "Max";
             speed_percentage = 90.0;
@@ -111,50 +112,22 @@ public class FullGameCode_V9 extends OpMode {
             speed_percentage = 25.0;
         }
 
-        /* Uses left joystick to go forward, backward, left, and right, and right joystick to rotate.
-
-           Left joystick up --> forward
-
-           Left joystick down --> Backward
-
-           Left joystick right --> Right
-
-           Left joystick left --> Left
-
-           Right joystick left --> rotate left
-
-           Right joystick right --> rotate right
-
-        */
-
-        double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+        /// Drivetrain controls.
+        double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives a negative value.
         double lateral =  gamepad1.left_stick_x;
         double yaw     =  gamepad1.right_stick_x;
-        /*
-        boolean goForward  = gamepad1.dpad_up;
-        boolean goBackward = gamepad1.dpad_down;
-        boolean goLeft  = gamepad1.dpad_left;
-        boolean goRight  = gamepad1.dpad_right;
-        */
-
-
 
         drive.setDriveMotorPower(axial, lateral, yaw, speed_percentage);
 
-
-        //Display Runtime
-        telemetry.addData("Status", "Run Time: " + runtime);
-        //Displaying Axial,Lateral,Yaw AKA horizontal, vertical, and spin
-        telemetry.addData("Axial:", axial);
-        telemetry.addData("Lateral:",lateral);
-        telemetry.addData("Yaw:", yaw);
+        ///Miscellaneous telemetries.
+        //Displaying runtime.
+        telemetry.addData("Status", "Run Time: " + runtime.seconds());
+        //Displaying current speed.
         telemetry.addData("Current Speed", speedCap);
-        telemetry.addData("Speed percentage: ",speed_percentage);
 
-
-        /// intake mechanism ----------------------------------------------------------------------
-
+        /// Intake mechanisms.
         double intakeArmPower = gamepad2.left_stick_y * 0.5;
+
         intakeArm.setPower(intakeArmPower);
 
         if (gamepad2.right_stick_x > 0) {
@@ -165,73 +138,51 @@ public class FullGameCode_V9 extends OpMode {
             claw.setPower(0.0);
         }
 
-        telemetry.addData("intake arm power: ", intakeArmPower);
-
-        telemetry.addData("claw power: ", claw.getPower());
-
-        /// Long arm mechanism ------------------------------------------------------------------
-
-        /// Long arm ---------------------------------------------------------
-
+        /// Linear slide.
         double armPower;
-        boolean noMove;
 
         if (gamepad2.left_trigger > 0) {
             armPower = -gamepad2.left_trigger;
-            noMove = false;
         } else if (gamepad2.right_trigger > 0) {
             armPower = gamepad2.right_trigger;
-            noMove = false;
         } else {
             armPower = 0.065;
-            noMove = true;
         }
 
         leftArmLift.setPower(armPower);
         rightArmLift.setPower(armPower);
 
-        /// Basket -----------------------------------------------------------
-
+        /// Basket.
+        //Manual controls.
         if (gamepad2.dpad_down) {
             basket.setPosition(1); // Drop
         } else if (gamepad2.dpad_up) {
             basket.setPosition(0.55); // Collect
-        } else if (gamepad2.dpad_left) {
-            basket.setPosition(1);
         } else if (gamepad2.dpad_right) {
             basket.setPosition(0.45); // Move
         }
 
+        //Automatic controls.
         if (armPower > 0.065 || armPower < 0.065) {
             basket.setPosition(0.45);
-        }
-        if (intakeArmPower < 0 && gamepad2.left_trigger == 0 && gamepad2.right_trigger == 0) {
+        } else if (intakeArmPower < 0 && gamepad2.left_trigger == 0 && gamepad2.right_trigger == 0) {
             basket.setPosition(0.55);
         }
 
-
-
-        telemetry.addData("intake arm power: ", armPower);
-
-        telemetry.addData("basket position: ", basket.getPosition());
-
-        /// Long Arm Slight Raise -----------------------------------------------
-
-
-
-        /// Long Arm Slight Raise -----------------------------------------------
-
-        ///Sensors --------------------------------------------------------------
+        /// Color Sensor.
         String colorOfBlock = color.blockColor();
+
+        //Telemetry
         if (color.getDistance(DistanceUnit.INCH) < 0.5) {
-            telemetry.addData("Block Color", color.blockColor());
+            telemetry.addData("Sample Color", color.blockColor());
         }
 
-        if (redSpy) {
+        //Automatic sample rejection system.
+        if (Objects.equals(redSpy, "red")) {
             if (Objects.equals(colorOfBlock, "blue")) {
                 rightColor = false;
             }
-        } else if (redSpy = false) {
+        } else if (Objects.equals(redSpy, "blue")) {
             if (Objects.equals(colorOfBlock, "red")) {
                 rightColor = false;
             }
@@ -239,13 +190,7 @@ public class FullGameCode_V9 extends OpMode {
             rightColor = true;
         }
 
-
+        ///REV Blinkin.
         light.sample(redSpy);
     }
 }
-
-
-
-
-
-
