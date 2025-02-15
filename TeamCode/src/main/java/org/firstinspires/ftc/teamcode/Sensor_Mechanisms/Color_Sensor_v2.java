a566546c425615a781cb02c40943d7858d7581b7
NotQuite8Bit <advaith@tcrobotics.org>
2025-02-15 13:22:45 -0500

    Color sensor stuff.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensor_Mechanisms/Color_Sensor_v2.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.Sensor_Mechanisms;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@Disabled
+public class Color_Sensor_v2 {
+    private ColorSensor color;
+    private DistanceSensor distance;
+
+    public void init (HardwareMap hardwareMap) {
+        color = hardwareMap.get(ColorSensor.class, "color");
+        distance = hardwareMap.get(DistanceSensor.class, "color");
+    }
+
+    public int red() {
+        return color.red();
+    }
+
+    public int blue() {
+        return color.blue();
+    }
+    public int yellow() {
+        return color.green();
+    }
+    public String sampleColor() {
+        int red = red();
+        int blue = blue();
+        int yellow = yellow();
+        String color = "none";
+
+        if (red > 500 || blue > 500 || yellow > 500) {
+            return "collected";
+        }
+        if (distance.getDistance(DistanceUnit.INCH) < 2) {
+            if (red > 100 && red > blue && red > yellow) {
+                color = "red";
+            } else if (blue > 100 && blue > red && blue > yellow) {
+                color = "blue";
+            } else if (yellow > 100 && yellow > blue && yellow > red) {
+                color = "yellow";
+            }
+        }
+        return color;
+    }
+}
